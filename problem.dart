bc8e8b37e31bf817223952f4dd7b0a80648874aa
Shahriar Alif <fahim.shahriar.alif@gmail.com>
2026-07-08 01:50:23 +0600

    solve 2

--- problem.dart
+++ problem.dart
@@ -1,31 +1,65 @@
-class Media {
-  String title;
-  int releaseYear;
+// class Media {
+//   String title;
+//   int releaseYear;
 
-  Media(this.title, this.releaseYear);
+//   Media(this.title, this.releaseYear);
+
+//   String getDetails() {
+//     return "$title ($releaseYear)";
+//   }
+// }
+
+// class Movie extends Media {
+//   double imdbRating;
+//   String director;
+
+//   // Build the constructor using super, as we covered in class
+//   Movie(String title, int releaseYear, this.imdbRating, this.director)
+//     : super(title, releaseYear);
+
+//   // Override getDetails()
+//   @override
+//   String getDetails() {
+//     // Your code here
+//     return "${super.getDetails()} , rating $imdbRating, directed by $director";
+//   }
+
+//   // Add an isHighlyRated() function that returns true if imdbRating is greater than or equal to 8.0
+//   bool isHighlyRated() {
+//     return imdbRating >= 8.0;
+//   }
+// }
+
+class Software {
+  String name;
+  String developer;
+
+  Software(this.name, this.developer);
 
   String getDetails() {
-    return "$title ($releaseYear)";
+    return "$name by $developer";
   }
 }
 
-class Movie extends Media {
-  double imdbRating;
-  String director;
+class App extends Software {
+  double sizeMB;
+  String platform;
 
   // Build the constructor using super, as we covered in class
-  Movie(String title, int releaseYear, this.imdbRating, this.director)
-    : super(title, releaseYear);
+
+  App(String name, String developer, this.sizeMB, this.platform)
+    : super(name, developer);
 
   // Override getDetails()
   @override
   String getDetails() {
     // Your code here
-    return "${super.getDetails()} , rating $imdbRating, directed by $director";
+    return " ${super.getDetails()} , Size = $sizeMB , Platform = $platform ";
   }
 
-  // Add an isHighlyRated() function that returns true if imdbRating is greater than or equal to 8.0
-  bool isHighlyRated() {
-    return imdbRating >= 8.0;
+  // Add an isHeavyApp() function that returns true if sizeMB is greater than 500.0
+
+  bool isHeavyApp() {
+    return sizeMB > 500;
   }
 }
