efbd2b29216645f79c68e0f030772fba2030f5ac
Shahriar Alif <fahim.shahriar.alif@gmail.com>
2026-07-10 22:30:07 +0600

    p6 solved

---
+++ Problems/p6.dart
@@ -0,0 +1,36 @@
+class Service {
+  String serviceName;
+  double monthlyFee;
+
+  Service(this.serviceName, this.monthlyFee);
+
+  String getDetails() {
+    return "$serviceName (\$${monthlyFee}/mo)";
+  }
+}
+
+class Subscription extends Service {
+  int daysRemaining;
+  String accountTier;
+
+  // Build the constructor using super, as we covered in class
+  Subscription(
+    String serviceName,
+    double monthlyFee,
+    this.daysRemaining,
+    this.accountTier,
+  ) : super(serviceName, monthlyFee);
+
+  // Override getDetails()
+  @override
+  String getDetails() {
+    // Your code here
+    return " ${super.getDetails} , Days remained $daysRemaining , account tier $accountTier ";
+  }
+
+  // Add an isExpiringSoon() function that returns true if daysRemaining is less than or equal to 7
+
+  bool isExpiringSoon() {
+    return daysRemaining <= 7;
+  }
+}
